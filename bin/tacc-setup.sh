67da4fb1ec0e045972cac8de2d6ddeaf48553279
Wesley Bomar <wbomar@tacc.utexas.edu>
2024-05-03 18:43:52 -0500

    refactor: simpler way to ensure a tacc-readthedocs

--- bin/tacc-setup.sh
+++ bin/tacc-setup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+mkdir -p ./user-guide/themes/tacc-readthedocs
+
 curl -o ./user-guide/mkdocs.base.yml https://cdn.jsdelivr.net/gh/DesignSafe-CI/DS-User-Guide@e0a3bd0/user-guide/mkdocs.base.yml
 curl -o ./poetry.lock https://cdn.jsdelivr.net/gh/DesignSafe-CI/DS-User-Guide@26257aa/poetry.lock
 curl -o ./pyproject.toml https://cdn.jsdelivr.net/gh/DesignSafe-CI/DS-User-Guide@26257aa/pyproject.toml
