cc057b50e5225ed05d4b5e0232fa3612507c7894
Wesley Bomar <wbomar@tacc.utexas.edu>
2024-05-03 19:15:46 -0500

    style: cleaner diff

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,10 @@
 FROM tup_docs-2024-05-03t16-46
 
+# To archive TACC code, before replacing it
 RUN mv /code /code-from-tacc
 COPY ./user-guide/ /code/
+
+# To restore TACC assets for theme
 RUN cp -r /code-from-tacc/docs/js/core /code/docs/js/core && \
     cp -r /code-from-tacc/docs/js/autoScrollNav.js /code/docs/js/autoScrollNav.js && \
     cp -r /code-from-tacc/docs/css/core /code/docs/css/core
--- bin/tacc-setup.sh
+++ bin/tacc-setup.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+# To avoid the error:
+# > ERROR - Config value 'theme': The path set in custom_dir
+# >         ('.../user-guide/themes/tacc-readthedocs') does not exist.
 mkdir -p ./user-guide/themes/tacc-readthedocs
 
+# To clone TACC files (so authors can preview without Docker)
 curl -o ./user-guide/mkdocs.base.yml https://cdn.jsdelivr.net/gh/DesignSafe-CI/DS-User-Guide@e0a3bd0/user-guide/mkdocs.base.yml
 curl -o ./poetry.lock https://cdn.jsdelivr.net/gh/DesignSafe-CI/DS-User-Guide@26257aa/poetry.lock
 curl -o ./pyproject.toml https://cdn.jsdelivr.net/gh/DesignSafe-CI/DS-User-Guide@26257aa/pyproject.toml
