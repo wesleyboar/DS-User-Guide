40d3abf5f99f8fea4e7901c16760c54707d830af
Wesley Bomar <wbomar@tacc.utexas.edu>
2024-05-06 16:54:04 -0500

    feat: provide TACC/TACC-Docs files on public repo
    TACC/TACC-Docs is not public yet.
    
    Lazy option is host on DS in a branch.
    
    Other option is a temporary public repo on wesleyboar or TACC.

--- bin/tacc-setup.sh
+++ bin/tacc-setup.sh
@@ -6,7 +6,7 @@
 mkdir -p ./user-guide/themes/tacc-readthedocs
 
 # To clone TACC files (so authors can preview without Docker)
-# TODO: Make TACC/TACC-Docs public or until then, provide a temporary CDN
-curl -o ./user-guide/mkdocs.base.yml https://cdn.jsdelivr.net/gh/TACC/TACC-Docs@e7dfaac/mkdocs.base.yml
-curl -o ./poetry.lock https://cdn.jsdelivr.net/gh/TACC/TACC-Docs@e7dfaac/poetry.lock
-curl -o ./pyproject.toml https://cdn.jsdelivr.net/gh/TACC/TACC-Docs@e7dfaac/pyproject.toml
+# TODO: Make TACC/TACC-Docs public, so we can load from TACC/TACC-Docs via CDN
+curl -o ./user-guide/mkdocs.base.yml https://cdn.jsdelivr.net/gh/DesignSafe-CI/DS-User-Guide@tacc/tacc-docs/user-guide/mkdocs.base.yml
+curl -o ./poetry.lock https://cdn.jsdelivr.net/gh/DesignSafe-CI/DS-User-Guide@tacc/tacc-docs/poetry.lock
+curl -o ./pyproject.toml https://cdn.jsdelivr.net/gh/DesignSafe-CI/DS-User-Guide@tacc/tacc-docs/pyproject.toml
