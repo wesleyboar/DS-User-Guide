412eff985ae002bb0b0faed394d1d8fe9db4253d
Wesley Bomar <wbomar@tacc.utexas.edu>
2024-06-03 16:51:49 -0500

    feat: add n' use variable BASE_URL for TACC files

--- bin/tacc-setup.sh
+++ bin/tacc-setup.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# To provide files via a CDN from a specific repo at a specific commit-ish
+# https://github.com/DesignSafe-CI/DS-User-Guide/pull/14
+BASE_URL="https://cdn.jsdelivr.net/gh/DesignSafe-CI/DS-User-Guide@c9340d6"
+
 # To avoid the error:
 # > ERROR - Config value 'theme': The path set in custom_dir
 # >         ('.../user-guide/themes/tacc-readthedocs') does not exist.
@@ -7,6 +11,6 @@ mkdir -p ./user-guide/themes/tacc-readthedocs
 
 # To clone TACC files (so authors can preview without Docker)
 # TODO: Make TACC/TACC-Docs public, so we can load from TACC/TACC-Docs via CDN
-curl -o ./user-guide/mkdocs.base.yml https://cdn.jsdelivr.net/gh/DesignSafe-CI/DS-User-Guide@tacc/tacc-docs/user-guide/mkdocs.base.yml
-curl -o ./poetry.lock https://cdn.jsdelivr.net/gh/DesignSafe-CI/DS-User-Guide@tacc/tacc-docs/poetry.lock
-curl -o ./pyproject.toml https://cdn.jsdelivr.net/gh/DesignSafe-CI/DS-User-Guide@tacc/tacc-docs/pyproject.toml
+curl -o ./user-guide/mkdocs.base.yml ${BASE_URL}/user-guide/mkdocs.base.yml
+curl -o ./poetry.lock ${BASE_URL}/poetry.lock
+curl -o ./pyproject.toml ${BASE_URL}/pyproject.toml
