9a242f4379ea926045b3ec69309e3797a9810b01
Wesley Bomar <wbomar@tacc.utexas.edu>
2024-06-05 11:58:04 -0500

    chore: update tacc-docs files commit

--- bin/tacc-setup.sh
+++ bin/tacc-setup.sh
@@ -2,7 +2,7 @@
 
 # To provide files via a CDN from a specific repo at a specific commit-ish
 # https://github.com/DesignSafe-CI/DS-User-Guide/pull/14
-BASE_URL="https://cdn.jsdelivr.net/gh/DesignSafe-CI/DS-User-Guide@c9340d6"
+BASE_URL="https://cdn.jsdelivr.net/gh/DesignSafe-CI/DS-User-Guide@016dcda"
 
 # To avoid the error:
 # > ERROR - Config value 'theme': The path set in custom_dir
